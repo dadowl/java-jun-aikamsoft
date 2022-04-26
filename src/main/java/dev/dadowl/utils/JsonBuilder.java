11dc24fd30194cbfc754129764bf93b38e9f8114
DadOwl <me@dadowl.dev>
2022-04-26 13:41:04 +0700

    add json builder

---
+++ src/main/java/dev/dadowl/utils/JsonBuilder.java
@@ -0,0 +1,43 @@
+package dev.dadowl.utils;
+
+import com.google.gson.JsonObject;
+
+public class JsonBuilder {
+
+    private JsonObject json = new JsonObject();
+
+    public JsonBuilder() {}
+
+    public JsonBuilder(JsonObject json){
+        this.json = json;
+    }
+
+    public JsonBuilder add(String key, String value) {
+        json.addProperty(key, value);
+        return this;
+    }
+
+    public JsonBuilder add(String key, int value) {
+        json.addProperty(key, value);
+        return this;
+    }
+
+    public JsonBuilder add(String key, boolean value) {
+        json.addProperty(key, value);
+        return this;
+    }
+
+    public JsonBuilder add(String key, JsonObject value) {
+        json.add(key, value);
+        return this;
+    }
+
+    public String toString() {
+        return json.toString();
+    }
+
+    public JsonObject build() {
+        return json;
+    }
+
+}
