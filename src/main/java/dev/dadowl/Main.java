3e54e34015ff1a089c520aa30512add6f649106e
DadOwl <me@dadowl.dev>
2022-05-01 15:17:50 +0700

    read the arguments and load input json

--- src/main/java/dev/dadowl/Main.java
+++ src/main/java/dev/dadowl/Main.java
@@ -1,9 +1,39 @@
 package dev.dadowl;
 
+import com.google.gson.JsonElement;
+import dev.dadowl.utils.FileUtil;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static RequestType REQUEST_TYPE = RequestType.NONE;
+    public static JsonElement INPUT_FILE;
+    public static String OUTPUT_FILE = "";
 
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3){
+            System.out.println("Incomplete arguments");
+            return;
+        }
+        try {
+            REQUEST_TYPE = RequestType.valueOf("search".toUpperCase()/*args[0].toUpperCase()*/);
+        } catch (Exception e){
+            System.out.println("Request type is invalid");
+            return;
+        }
+
+        INPUT_FILE = FileUtil.openFile(args[1]);
+        if (INPUT_FILE == null){
+            throw new Exception("The input file is not found or JSON error!");
+        }
+        if (INPUT_FILE.isJsonNull()){
+            throw new Exception("The input file is empty!");
+        }
+
+        OUTPUT_FILE = args[2];
+        if (OUTPUT_FILE.isEmpty()){
+            System.out.println("Output file entered incorrectly");
+            return;
+        }
     }
 
 }
